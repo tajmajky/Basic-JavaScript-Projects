0be445240ed394089b7c5be68edc900b5b499f20
Timea Toth <113320355+tajmajky@users.noreply.github.com>
2023-02-06 23:12:18 +0000

    beginner JS

--- Project1_expression_alert/JS/JSmain.js
+++ Project1_expression_alert/JS/JSmain.js
@@ -1,3 +1,29 @@
+//alert
 window.alert("Hello, World!");
 
-document.write("Hi");
+//doc write
+document.write("Hi<br><br>");
+
+//var string
+var A ="This is a string<br>";
+document.write(A);
+
+
+//alert
+var x ="Timea";
+window.alert(x);
+
+//escaping characters
+document.write("<br>Lisa told Bart, \"Knock it off Bart\"\, don\`t\ ...");
+
+//concatenated strings - quotation
+document.write("<br><br>\"Be who you are and say what you feel,"+
+ "because those who mind don\'t matter and those who matter don\'t mind.\"" +"-Dr. Seuss<br>");
+
+ //concatenated strings
+ var B = "<br>concat" + "string<br><br>"
+ document.write(B)
+
+//multiple variables
+ var Family="The Team", Mom = "Maria", Dad = "Greg", Daughter = "Jessie", Son="Jim";
+ document.write(Son);
