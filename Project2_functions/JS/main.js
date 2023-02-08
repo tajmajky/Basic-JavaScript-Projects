ef161c9b139e6de2240d64d96a0b3d4f3dd15ecf
Timea Toth <113320355+tajmajky@users.noreply.github.com>
2023-02-08 16:11:01 +0000

    += assignment

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,7 +1,15 @@
+//adding  two numbers
 function myResult() {
     var a = parseInt(document.getElementById("alpha").value);
     var b = parseInt(document.getElementById("beta").value);
 
     var result= a + b;
     alert(result);
+  }
+
+  //concat 2 strings on a button click
+  function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book.";
+    document.getElementById("Concatenate").innerHTML = sentence;
   }
