a2b675eead4e398f9270356ac938d0d3ceff53cf
Timea Toth <113320355+tajmajky@users.noreply.github.com>
2023-02-16 23:58:25 +0000

    assignm subm 7

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -29,3 +29,61 @@ function Add_numbers_4(){ //var x is NOT in function so calculation is NOT done
 Add_numbers_3();
 Add_numbers_4();
 
+function get_Date() {
+    if (new Date().getHours() > 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+
+
+//My Own If Statement
+
+function Restriction() {
+    var age = document.getElementById("Age").value;
+    if (age >= 18) {
+        document.getElementById("Enter").innerHTML = "You can attend this event";
+    }
+}
+
+//Else statement
+function Age2_Function() {
+    let Vote;
+    let Age2 = document.getElementById("Age2").value;
+    if(Age2 >= 18) {
+        Vote ="You are old enough to vote!";
+    }
+    else {
+        Vote="You are not old enough to  vote!";
+    }  
+    document.getElementById("How_old_are_you?").innerHTML = Vote; 
+}
+
+//My Own If-Else Statement
+function days_Function() {
+    let howMany;
+    let days = document.getElementById("days").value;
+    if(days == 31) {
+        howMany ="That is correct! Well done!";
+    }
+    else {
+        howMany="Wrong answer. Try again!";
+    }  
+    document.getElementById("january").innerHTML = howMany; 
+}
+
+//Else If Statements
+function Time_function() {
+    var Time= new Date().getHours();
+    var Reply;
+    if(Time < 12 == Time > 0) {
+        Reply = "It is morning time";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is the afternoon.";
+    }
+    else {
+        Reply = "It is evening time";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
