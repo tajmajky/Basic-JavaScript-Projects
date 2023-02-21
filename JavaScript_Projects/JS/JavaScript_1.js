75e6a3e897d966d637187627a53d1218ec0cbe85
Timea Toth <113320355+tajmajky@users.noreply.github.com>
2023-02-21 00:31:28 +0000

    Switch/break Statement, Class, Canvas

--- JavaScript_Projects/JS/JavaScript_1.js
+++ JavaScript_Projects/JS/JavaScript_1.js
@@ -29,3 +29,36 @@ function colorFunc() {
     }
     document.getElementById("Output").innerHTML = ColorOutput;
 }
+
+
+
+//assign 57
+//document.getElementsByClassName() method
+
+function helloWorldFunc() {
+var A=document.getElementsByClassName("Click");
+A[0].innerHTML = "The text has changed."; //chose the 0 index to make changes
+
+}
+
+
+//gradient filling canvas
+
+var c = document.getElementById("idName");
+var ctx = c.getContext("2d");
+
+var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 400);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(0,0,500,250);
+
+
+//circle in canvas
+
+var c = document.getElementById("graphic");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
