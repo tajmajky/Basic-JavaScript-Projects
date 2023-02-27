34cc9e7cfcfd97842b2c2fcae0193ad9b0c25167
Timea Toth <113320355+tajmajky@users.noreply.github.com>
2023-02-27 22:56:38 +0000

    arrow()

---
+++ Tic Tac Toe/JS/arrow.js
@@ -0,0 +1,11 @@
+let x = 3;
+let y = 45;
+myStuff = () => document.getElementById("multip").innerHTML = (x*y);
+
+function valami () {
+    let v = "sadf";
+    let q = "fdffffff";
+    let g = v+q;
+    document.getElementById("hey").innerHTML = g;
+
+}
