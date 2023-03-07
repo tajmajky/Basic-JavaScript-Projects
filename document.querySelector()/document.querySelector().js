186c86805b748b08665766f4d73760de04eee9bd
Timea Toth <113320355+tajmajky@users.noreply.github.com>
2023-03-07 00:11:11 +0000

    calculator

---
+++ document.querySelector()/document.querySelector().js
@@ -0,0 +1,11 @@
+//document.querySelector() Method
+
+//The document.querySelector() method returns the first element within a file 
+//that matches the selector specified. 
+//If there are no matches found, null is returned. 
+//If you wanted to return a group of elements, 
+//you would use the document.querySelectorAll() method. 
+
+//Example
+//document.querySelector(“p”) would return only the first paragraph element in a document.
+// document.querySelectorAll(“p”) would return all paragraph elements in a document.
